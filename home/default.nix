4c8a7bbd2bd90311d5130a3652973870e5486fe4
novel2430 <novel2430@163.com>
2024-05-09 15:53:26 +0800

    delete garbage

--- home/default.nix
+++ home/default.nix
@@ -42,63 +42,6 @@
     ])
     ;
 
-  # git
-  # programs.git = {
-  #   enable = true;
-  #   userName = "novel2430";
-  #   userEmail = "novel2430@163.com";
-  # };
-  # Bash
-  # programs.bash = {
-  #   enable = true;
-  #   enableCompletion = true;
-  #   # TODO 在这里添加你的自定义 bashrc 内容
-  #   bashrcExtra = lib.mkMerge [
-  #     (lib.mkIf (opt-config.use-proxy == true) ''
-  #       export http_proxy="${opt-config.http-proxy}"
-  #       export https_proxy="${opt-config.https-proxy}"
-  #     '')
-  #     # Other Stuff
-  #     (''
-  #     '')
-  #   ];
-  #   #shellAliases = {
-  #   #};
-  # };
-  # Zsh
-  # programs.zsh = {
-  #   enable = true;
-  #   enableAutosuggestions = true;
-  #   oh-my-zsh.enable = true;
-  #   oh-my-zsh.plugins = [ "git" ];
-  #   oh-my-zsh.theme = "robbyrussell";
-  #   syntaxHighlighting.enable = true;
-  #   initExtra = lib.mkMerge [
-  #     (lib.mkIf (opt-config.use-proxy == true) ''
-  #       export http_proxy="${opt-config.http-proxy}"
-  #       export https_proxy="${opt-config.https-proxy}"
-  #     '')
-  #     # Other Stuff
-  #     (''
-  #       export EDITOR="nvim"
-  #     '')
-  #     # FZF Stuff
-  #     (''
-  #       export FZF_COMPLETION_TRIGGER='\'
-  #       # Use fd (https://github.com/sharkdp/fd) for listing path candidates.
-  #       # - The first argument to the function ($1) is the base path to start traversal
-  #       # - See the source code (completion.{bash,zsh}) for the details.
-  #       _fzf_compgen_path() {
-  #         fd --follow --exclude ".git" . "$1"
-  #       }
-  #
-  #       # Use fd to generate the list for directory completion
-  #       _fzf_compgen_dir() {
-  #         fd --type d --follow --exclude ".git" . "$1"
-  #       }
-  #     '')
-  #   ];
-  # };
   # XDG_DATA_DIRS
   xdg.systemDirs.data = [
     "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
