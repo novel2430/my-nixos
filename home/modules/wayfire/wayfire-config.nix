e976fc7410e74ffdfc85024857928183438e9209
novel2430 <novel2430@163.com>
2024-05-09 19:17:53 +0800

    add wayfire-config

--- home/modules/wayfire/wayfire-config.nix
+++ home/modules/wayfire/wayfire-config.nix
@@ -3,24 +3,24 @@ with lib;
 let
   wayfire-config = {
     alpha = {
-      min_value = 0.100000;
+      min_value = "0.100000";
       modifier = "<alt> <super>";
     };
     animate = {
       close_animation = "zoom";
-      duration = 400;
+      duration = "400";
       enabled_for = ''(type equals "toplevel" | (type equals "x-or" & focusable equals true))'';
-      fade_duration = 400;
+      fade_duration = "400";
       fade_enabled_for = ''type equals "overlay"'';
       fire_color = "#B22303FF";
-      fire_duration = 300;
+      fire_duration = "300";
       fire_enabled_for = "none";
-      fire_particle_size = 16.000000;
-      fire_particles = 2000;
+      fire_particle_size = "16.000000";
+      fire_particles = "2000";
       open_animation = "zoom";
       random_fire_color = "false";
-      startup_duration = 600;
-      zoom_duration = 500;
+      startup_duration = "600";
+      zoom_duration = "500";
       zoom_enabled_for = "none";
     };
   };
