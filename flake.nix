f5af378c82237beb7b2cb161677d2f1a20168736
novel2430 <novel2430@163.com>
2024-05-10 14:25:22 +0800

    update

--- flake.nix
+++ flake.nix
@@ -61,7 +61,7 @@
       config.permittedInsecurePackages = allowed-insecure-packages;
     };
 
-    uu = nixpkgs-unstable.legacyPackages.${system}.override {
+    uu = nixpkgs-unstable.override {
       overlays = [ nur.overlay ]; 
     };
 
