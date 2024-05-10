134acf21837e3e98bd8b9d546bceeab7751bdf8d
novel2430 <novel2430@163.com>
2024-05-10 12:30:36 +0800

    update

--- internet-src/default.nix
+++ internet-src/default.nix
@@ -1,8 +1,8 @@
 {
   wemeetSrc = rec {
-    name = "wemeet-bin";
-    version = "3.19.0.401";
-    url = "https://updatecdn.meeting.qq.com/cos/bb4001c715553579a8b3e496233331d4/TencentMeeting_0300000000_${version}_x86_64_default.publish.deb";
+    # name = "wemeet-bin";
+    # version = "3.19.0.401";
+    url = "https://updatecdn.meeting.qq.com/cos/bb4001c715553579a8b3e496233331d4/TencentMeeting_0300000000_3.19.0.401_x86_64_default.publish.deb";
     flake = false;
   };
 }
