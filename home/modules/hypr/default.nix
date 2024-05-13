49b9e0d2f8f7edb6cba780ef778286aecfc4b3b8
novel2430 <novel2430@163.com>
2024-05-14 01:05:06 +0800

    update

--- home/modules/hypr/default.nix
+++ home/modules/hypr/default.nix
@@ -95,6 +95,16 @@
         new_is_master = true;
         new_on_top = true;
       };
+      env = [
+        "LIBVA_DRIVER_NAME,nvidia"
+        "XDG_SESSION_TYPE,wayland"
+        "GBM_BACKEND,nvidia-drm"
+        "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+        "WLR_NO_HARDWARE_CURSORS,1"
+      ];
+      exec = [
+        "dunstify 'HIIIIIIIIII'"
+      ];
     };
   };
 }
