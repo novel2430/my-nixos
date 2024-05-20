58a53e48fe4242b3b0d37a3dfc420f1563e1cd44
novel2430 <novel2430@163.com>
2024-05-20 21:27:02 +0800

    update

---
+++ hosts/ASUSTek/options.nix
@@ -0,0 +1,56 @@
+{
+  opt-config = rec {
+    # Basic
+    username = "novel2430";
+
+    # Git
+    gitname = "novel2430";
+    gitmail = "novel2430@163.com";
+    gitbranch = "main";
+
+    # Screenshots Directory
+    screenshot-dir = "/home/${username}/Pictures/screenshot";
+    # Wallpaper
+    lock-img = "/home/${username}/.config/wayfire/lock.png";
+    # Lock Paper
+    wall-img = "/home/${username}/.config/wayfire/wall.png";
+
+    # intel, nvidia, intel-nvidia
+    gpu-type = "intel";
+    intel-bus-id = "PCI:0:2:0";
+    nvidia-bus-id = "PCI:3:0:0";
+
+    # Monitors (For Wayfire Config)
+    # Example :
+    # "output:<monitor-name>" = {
+    #   depth = "8";
+    #   mode = "off";
+    #   position = "auto";
+    #   scale = "1.0";
+    #   transform = "normal";
+    #   vrr = "false";
+    # }
+    monitors = {
+    };
+
+    # Use Clash
+    use-clash = false;
+    clash-dir = "/home/${username}/clash";
+    # Proxy
+    use-proxy = false;
+    http-proxy-host = "127.0.0.1";
+    http-proxy-port = "7890";
+    https-proxy-host = "127.0.0.1";
+    https-proxy-port = "7890";
+    http-proxy = "http://${http-proxy-host}:${http-proxy-port}";
+    https-proxy = "http://${https-proxy-host}:${https-proxy-port}";
+
+    # ZJU RVPN
+    use-zju-rvpn = false;
+    zju-rvpn-port = "7895";
+    zju-rvpn-config = "/home/${username}/zjuconnect/config.toml";
+
+    # In China
+    in-china = false;
+  };
+}
