1c3a3ccc09bd05634e4d6779ed24adaa224da4a1
novel2430 <novel2430@163.com>
2024-05-21 00:33:30 +0800

    update

--- home/modules/wayfire/scripts/swayidle.nix
+++ home/modules/wayfire/scripts/swayidle.nix
@@ -4,8 +4,8 @@ let
   swayidle = "${pkgs.swayidle}/bin/swayidle";
 in
 pkgs.writeShellScriptBin "my-swayidle" ''
-  dpms_off_cmd="${wlopm} --off HDMI-A-1 ; ${wlopm} --off eDP-1; ${wlopm} --off VGA-1; ${wlopm} --off LVDS-1"
-  dpms_on_cmd="${wlopm} --on HDMI-A-1 ; ${wlopm} --on eDP-1; ${wlopm} --on VGA-1; ${wlopm} --on LVDS-1"
+  dpms_off_cmd="${wlopm} --off HDMI-A-1 ; ${wlopm} --off eDP-1; ${wlopm} --off VGA-1; ${wlopm} --off LVDS-1; ${wlopm} --off HDMI-A-2"
+  dpms_on_cmd="${wlopm} --on HDMI-A-1 ; ${wlopm} --on eDP-1; ${wlopm} --on VGA-1; ${wlopm} --on LVDS-1; ${wlopm} --on HDMI-A-2"
   lock_cmd="my-swaylock idle &"
 
   ${swayidle} -w \
