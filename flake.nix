80c50838197034ad0df1f64effae4eb8ac320f7a
novel2430 <novel2430@163.com>
2024-06-30 11:06:26 +0800

    update

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
   description = "My NixOS flake";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     # Unstable
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # HomeManager
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # NUR
