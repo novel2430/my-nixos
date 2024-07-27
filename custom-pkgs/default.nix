48bde81f04f13c723202b1d0db84b5137e63f42b
novel2430 <novel2430@163.com>
2024-07-27 12:22:38 +0800

    update

--- custom-pkgs/default.nix
+++ custom-pkgs/default.nix
@@ -1,7 +1,7 @@
 {pkgs, unstable-pkgs, ... }:
 {
   wemeet-bin-bwrap = pkgs.callPackage ./wemeet-bin-bwrap { };
-  wechat-universal-bwrap = unstable-pkgs.callPackage ./wechat-universal-bwrap { };
+  wechat-universal-bwrap = pkgs.callPackage ./wechat-universal-bwrap { };
   vscode-nvim = import ./vscode-nvim { pkgs=pkgs; };
   wpsoffice = pkgs.libsForQt5.callPackage ./wpsoffice { };
   gtk4-12-5 = pkgs.callPackage ./gtk4-12-5 { };
